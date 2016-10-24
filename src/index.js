3b35f534962363de7e3b8bcf472ffffbb52565c9
sunjing1809 <sunjing1809@gmail.com>
2016-10-24 14:43:53 -0500

    Fix test issue

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import Player from './components/Player';
 import Video from './components/Video';
-import ControlBar from './components/control-bar/ControlBar';
 import BigPlayButton from './components/BigPlayButton';
 import LoadingSpinner from './components/LoadingSpinner';
 import PosterImage from './components/PosterImage';
@@ -9,7 +8,6 @@ import PosterImage from './components/PosterImage';
 export {
   Player,
   Video,
-  ControlBar,
   BigPlayButton,
   LoadingSpinner,
   PosterImage,
