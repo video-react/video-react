4d8c6dfd1b400dae3e4da8ae0ee2a90a8352427e
sunjing1809 <sunjing1809@gmail.com>
2016-11-03 21:47:26 -0500

    Implemented PlayProgressBar Component
    Implemented PlayProgressBar Component

--- src/components/control-bar/PlayProgressBar.js
+++ src/components/control-bar/PlayProgressBar.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import { formatTime } from '../../lib/utils';
 
+const propTypes = {
+  currentTime: PropTypes.number.isRequired,
+  duration: PropTypes.number.isRequired,
+  percentage: PropTypes.string,
+};
 
-export default function PlayProgressBar(props) {
+// Shows play progress
+export default function PlayProgressBar({ currentTime, duration, percentage }) {
   return (
-    <div className="video-react-play-progress-bar">
+    <div
+      data-current-time={formatTime(currentTime, duration)}
+      className="video-react-play-progress video-react-slider-bar"
+      style={{
+        width: percentage,
+      }}
+    >
+      <span className="video-react-control-text"><span>Progress</span>: {percentage}</span>
     </div>
   );
 }
+
+PlayProgressBar.propTypes = propTypes;
