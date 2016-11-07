4c3ea1b453c13d63decdf1630d80fb0b3f716e30
ForEverBean <xl.chen2008@gmail.com>
2016-11-06 21:31:41 -0600

    Update ControlBar with more components imported.

--- src/components/control-bar/ControlBar.js
+++ src/components/control-bar/ControlBar.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
+import ProgressControl from './ProgressControl';
+import PlayToggle from './PlayToggle';
+import ReplayControl from './ReplayControl';
+import FullscreenToggle from './FullscreenToggle';
+
 
 export default function ControlBar(props) {
   return (
     <div className="video-react-control-bar">
+      <PlayToggle {...props} />
+      <ReplayControl {...props} />
+      <ProgressControl {...props} />
+      <FullscreenToggle {...props} />
     </div>
   );
 }
