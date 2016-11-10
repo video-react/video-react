67337ab808888252033d1ea5db05c91263d68466
ForEverBean <xl.chen2008@gmail.com>
2016-11-09 22:19:58 -0600

    Add Popup component.

---
+++ src/components/popup/Popup.js
@@ -0,0 +1,39 @@
+import React, { Component, PropTypes } from 'react';
+
+const propTypes = {
+  player: PropTypes.object,
+  children: PropTypes.any,
+};
+
+class Popup extends Component {
+
+  constructor(props, context) {
+    super(props, context);
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  render() {
+    const { children } = this.props;
+    return (
+      <div
+        className="video-react-menu"
+        onClick={this.handleClick}
+      >
+        <div className="video-react-menu-content">
+          { children }
+        </div>
+      </div>
+    );
+  }
+
+}
+
+Popup.propTypes = propTypes;
+export default Popup;
+
---
+++ src/components/popup/PopupButton.js
@@ -0,0 +1,51 @@
+import React, { Component, PropTypes } from 'react';
+import classNames from 'classnames';
+import Popup from './Popup';
+
+const propTypes = {
+  inline: PropTypes.bool,
+  onClick: PropTypes.func.isRequired,
+  className: PropTypes.string,
+};
+
+const defaultProps = {
+  inline: true,
+};
+
+class PopupButton extends Component {
+
+  constructor(props, context) {
+    super(props, context);
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    const { onClick } = this.props;
+    onClick(event);
+  }
+
+
+  render() {
+    const { inline } = this.props;
+    return (
+      <div
+        className={classNames(this.props.className, {
+          'video-react-menu-button-inline': !!inline,
+          'video-react-menu-button-popup': !inline,
+        }, 'video-react-control video-react-button video-react-menu-button')}
+        role="button"
+        onClick={this.handleClick}
+      >
+        <Popup {...this.props} />
+      </div>
+    );
+  }
+
+}
+
+PopupButton.propTypes = propTypes;
+PopupButton.defaultProps = defaultProps;
+
+export default PopupButton;
+
