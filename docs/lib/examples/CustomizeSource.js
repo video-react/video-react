24d5413b981b09870827a9e84ebe7437589380c2
Jim Liu <junminliu@gmail.com>
2016-11-24 15:49:40 -0600

    change hls video url source

--- docs/lib/examples/CustomizeSource.js
+++ docs/lib/examples/CustomizeSource.js
@@ -5,11 +5,13 @@ import HLSSource from './HLSSource';
 export default (props) => {
   // Add customized HLSSource component into video-react Player
   // The Component with `isVideoChild` attribute will be added into `Video` component
+  // Please use this url if you test it from local:
+  // http://www.streambox.fr/playlists/x36xhzz/x36xhzz.m3u8
   return (
     <Player>
       <HLSSource
         isVideoChild
-        src="http://www.streambox.fr/playlists/x36xhzz/x36xhzz.m3u8"
+        src="//d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8"
       />
     </Player>
   );
