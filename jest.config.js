3fd9e0bfa0a81ce429ed9499038f92848cdd25dc
Tianxiao Wang <xiaoyuhen1994@gmail.com>
2019-01-27 23:51:29 +0800

    chore: update jest config (#211)
    * chore: upgrade jest and babel-jest
    
    * chore: generator coverage reporter
    
    * chore: make installed tree green
    
    * update CI config

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  setupFilesAfterEnv: ['<rootDir>src/setupTests.js'],
+  coverageReporters: ['html', 'text-summary', 'cobertura', 'lcov'],
+  collectCoverageFrom: ['src/**/*.js'],
+  coveragePathIgnorePatterns: [
+    'setupTests.js',
+  ],
+}
