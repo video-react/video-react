7fcf41b5a163c2c00c9a898111f2e073028ff058
Arianna Lanz <ariannalanz@gmail.com>
2020-04-06 17:52:14 -0400

    just change replay for rewind not for everything else

--- src/components/control-bar/ReplayControl.js
+++ src/components/control-bar/ReplayControl.js
@@ -1,7 +1,7 @@
 import ForwardReplayControl from './ForwardReplayControl';
 
 // Pass mode into parent function
-const ReplayControl = ForwardReplayControl('rewind');
+const ReplayControl = ForwardReplayControl('replay');
 ReplayControl.displayName = 'ReplayControl';
 
 export default ReplayControl;
