3a1802c2bb87794404825d1c411808c51c47fbd3
jesse <jessedp@gmail.com>
2021-12-10 11:53:37 -0500

    use unofficial enzyme-adapter-react-17 until it is updated

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,6 @@
 /* eslint import/no-extraneous-dependencies: "off" */
 import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 configure({ adapter: new Adapter() });
 
